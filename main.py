9e194d4d41a3e478f98e746a5880092dd7d9ca80
EnterNick <Ravil.Mirgayazov@yandex.ru>
2024-04-21 23:02:18 +0300

    2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame_widgets
-
+import gif_pygame
 from constants import *
 from buttons import Group_of_buttons
 import pygame
@@ -15,7 +15,9 @@ def main():
     planet_buttons = Group_of_buttons(screen, 0, 25, 150, 390, PLANETS, len(PLANETS), 'Times New Roman')
     mode_buttons = Group_of_buttons(screen, 200, 15, 300, 50, ['Описание', 'Физ. характеристики'], 2, 'Times New Roman',
                                     False)
+    mode_buttons.actual_button = 'Физ. характеристики'
     all_sprites.add(planet_buttons)
+
     while running:
         screen.fill((43, 43, 43))
         events = pygame.event.get()
@@ -23,13 +25,15 @@ def main():
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
-
         pygame_widgets.update(events)
-        planet_img = CLASSES_OF_PLANETS[planet_buttons.actual_button].image \
-            if mode_buttons.actual_button == 'Физ. характеристики' \
-            else CLASSES_OF_PLANETS[planet_buttons.actual_button].image
-        screen.blit(planet_img, (200, 100))
 
+        if mode_buttons.actual_button == 'Физ. характеристики':
+            img = CLASSES_OF_PLANETS[planet_buttons.actual_button].image
+        else:
+            img = CLASSES_OF_PLANETS[planet_buttons.actual_button].gif.blit_ready()
+            img.set_colorkey((255, 255, 255))
+
+        screen.blit(img, (200, 100))
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
